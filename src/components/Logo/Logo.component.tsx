70046ac29ed4fa129ed4ef36030e3e0dd027c0a7
vicotroshenko <otroshenko.vl@gmail.com>
2024-07-22 18:49:32 +0300

    feat: Add header with components Logo, PrimaryButton, NavList. Changed variables, added new mixins

---
+++ src/components/Logo/Logo.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Logo.scss';
+
+const Logo = () => {
+	return (
+		<a href='/' className='logo'>
+			<span className='logo_letter'>T</span>
+			<span>Tactix.</span>
+		</a>
+	)
+}
+
+export default Logo
--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../../components/Header/Header.component';
 
 
 function App() {
   return (
     <>
-      <header></header>
+      <Header />
       <main>
       </main>
       <footer></footer>
